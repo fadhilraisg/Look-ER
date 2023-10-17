6d8fb2d6ffbfb098ece8f2cda76afa4c42df4cdf
fadhilraisg <fraisgunawan@gmail.com>
2023-10-17 16:43:05 +0700

    Menambahkan file Main.java ke dalam repositori

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+/*Kelompok 6 :
+Arvyn Rezky Fahrezy
+Fadhil Rais Gunawan
+Fakhira Shafa Maheswari
+Muhammad Azka Alif Hartono
+Muhammad Qais Alfaris */
+
+import applicationPage.welcomePage;
+
+public class Main {
+    public static void main(String[] args) {
+        welcomePage justTest = new welcomePage();
+        
+    }
+}
+
